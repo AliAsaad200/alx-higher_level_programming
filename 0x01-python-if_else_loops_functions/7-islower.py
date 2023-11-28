773281d7b1bc77b97bbdd73379368c826bc90649
AliAsaad200 <aliahmedassad2003@gmail.com>
2023-11-28 21:44:21 +0200

    W

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    islower = __import__('7-islower').islower
-    print("{:d} is {}".format(c, "lower" if islower("a") else "upper"))
+  return ord('a') <= ord(c) <= ord('z')
