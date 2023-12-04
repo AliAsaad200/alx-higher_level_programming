e59765807400bc185dacebdaa7397b340ed18b71
AliAsaad200 <aliahmedassad2003@gmail.com>
2023-12-04 19:43:32 +0200

    W

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     my_new_string = ""
     for i in len(my_string):
-        if i != "c" or i != "C":
+        if my_string[i] != "c" and my_string[i] != "C":
             my_new_string += my_string[i]
     return my_new_string
