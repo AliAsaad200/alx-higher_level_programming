c8e75f037449b63a12e0ec98529873c8b78658af
AliAsaad200 <aliahmedassad2003@gmail.com>
2023-12-04 19:14:05 +0200

    W

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
