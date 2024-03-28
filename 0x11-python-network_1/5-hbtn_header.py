5fe70169f80422b04f121a97fddee0f4a718f65e
AliAsaad200 <aliahmedassad2003@gmail.com>
2024-03-28 17:29:40 +0200

    W

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,6 +8,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
+
+    with requests.get(url) as response:
+        x_request_id = response.headers.get('X-Request-Id')
+        print(x_request_id)
